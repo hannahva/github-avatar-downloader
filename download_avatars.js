439cc2f7d60561d910b78c3bfcdcb3a8971eb05c
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-03 22:23:02 +0000

    defined requestURL inside function

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,17 @@
+var GITHUB_USER = "hannahva";
+var GITHUB_TOKEN = "2f671d8449220469203b0a621b73d2793fab65ed";
+
 var request = require("request");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb){
-  request.get()
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
 
 getRepoContributors("jquery", "jquery", function(err, result){
   console.log("Errors: ", err);
   console.log("Result: ", result);
 });
+
