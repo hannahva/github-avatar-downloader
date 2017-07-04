e4d88c1c6a002e074d0094f2fda670fe10c4a284
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-04 00:31:58 +0000

    parsed JSON string into object/ called this object in callback function to log each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,17 +1,32 @@
-var GITHUB_USER = "hannahva";
-var GITHUB_TOKEN = "2f671d8449220469203b0a621b73d2793fab65ed";
 
 var request = require("request");
+
+var requestOptions = {
+  headers: {
+    "User-Agent": "GitHub Avatar Downloader - Student Project"
+  }
+};
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb){
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var requestURL = 'https://'+ process.env.GITHUB_USER + ':' + process.env.GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
   console.log(requestURL);
+  request.get(requestURL, requestOptions, function(error, response, body){
+    var data = JSON.parse(body);
+    cb(data);
+  })
 }
 
-getRepoContributors("jquery", "jquery", function(err, result){
-  console.log("Errors: ", err);
-  console.log("Result: ", result);
-});
+// getRepoContributors("jquery", "jquery", function(err, result){
+//   console.log("Errors: ", err);
+//   console.log("Result: ", result);
+// });
 
+getRepoContributors("jquery", "jquery", findAvatar);
+
+function findAvatar(someJSON){
+  someJSON.forEach(function(contributor){
+    console.log(contributor.avatar_url)
+  })
+}
